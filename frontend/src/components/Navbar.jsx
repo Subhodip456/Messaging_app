3a52a21adbf80f53bc982b5d007d6d0f12005d37
Subhodip456 <zaynsubhod17@gmail.com>
2023-01-08 12:15:28 +0530

    m

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,13 +3,13 @@ import {ChatContext} from '../Context/chatContext'
 
 
 const Navbar = () => {
-  const {Names} = useContext(ChatContext)
+  // const {Names} = useContext(ChatContext)
   return (
     <div className='navbar'>
       <span className='logo'>CHAT APP</span>
       <div className='user'>
         <img src='https://images.pexels.com/photos/13623424/pexels-photo-13623424.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' alt=''></img>
-        <span>Subhodip</span>
+        {/* <span>Subhodip</span> */}
         <button>LOGOUT</button>
       </div>
     </div>
