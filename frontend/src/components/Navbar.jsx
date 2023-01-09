7dbdb01d3ffcac4561f6151ea878dcbfd6f33640
Subhodip456 <zaynsubhod17@gmail.com>
2023-01-09 10:28:07 +0530

    new changes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,15 +1,30 @@
-import React,{useContext} from 'react'
-import {ChatContext} from '../Context/chatContext'
+import React,{useEffect,useState,axios} from 'react'
+import axios from "axios"
 
 
 const Navbar = () => {
-  const {Name} = useContext(ChatContext)
+  // const {Names} = useContext(ChatContext)
+  const [Names,setNames] = useState("");
+  const getName = async () => {
+    try{
+      let getName_response = await axios.get("https://messaging-api.onrender.com/Register")
+      setNames(getName_response.data.Name)
+      console.log("getName_response",getName_response.data)
+    }
+    catch(error){
+      console.log("error in fetching name",error)
+    }
+  }
+  useEffect(()=>{
+    getName()
+  },[Names])
+
   return (
     <div className='navbar'>
       <span className='logo'>CHAT APP</span>
       <div className='user'>
         <img src='https://images.pexels.com/photos/13623424/pexels-photo-13623424.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' alt=''></img>
-        <span>{Name}</span>
+        <span>{Names}</span>
         <button>LOGOUT</button>
       </div>
     </div>
